ce96bd592d24d07b48fe5c91720003f4112f0696
fabiendaguet <daguet77@gmail.com>
2026-05-07 17:14:55 -0400

    feat: create footer en ts file

--- frontend/src/components/ui/Footer.tsx
+++ frontend/src/components/ui/Footer.tsx
@@ -1,6 +1,46 @@
+import type { FooterSection } from "../../types/FooterTypes";
+
+const footerSections: FooterSection[] = [
+  {
+    title: "Explore",
+    links: ["Categories", "News"]
+  },
+  {
+    title: "Trust",
+    links: ["Reviews", "Help", "conditions of use"]
+  },
+  {
+    title: "Owners",
+    links: ["Bests Owners", "Online Owners"]
+  },
+];
+
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <>
+      <footer className="footer">
+        <div className="footerContainer">
+          <div>
+            <div className="footerBrand-title">GearShare Canada</div>
+            <p className="footerBrand-description">Louer du matériel localement, économiser, réduire le gaspillage et simplifier les projets.</p>
+          </div>
+          {footerSections.map((section,key) => 
+            <div key={key}>
+              <div className="footerSectionTitle">
+                {section.title}
+              </div>
+              <div className="footerLinks">
+                {section.links.map((link, key) => 
+                  <span key={key}>
+                    {link}
+                  </span>
+                )}
+              </div>
+            </div>
+          )}
+        </div>
+      </footer>
+    </>
   )
 }
 export default Footer;
